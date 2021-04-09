1171bb85f677c53e4f37f2230b76f3f2d1708fa6
satyammutha <satyammutha@gmail.com>
2021-04-10 01:48:59 +0530

    - Added Solution of Exercise in Module 2 - Basic concepts in Unity

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Use Time.deltaTime to make Magnitude per second Instead of Magnitude per Frame
+        //So, after putting Time.deltaTime it won't be Device dependent
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime ,
+                            0,
+                            Input.GetAxis("Vertical") * speed * Time.deltaTime );
+    }
+}
